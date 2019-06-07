94952394a5cb1d0102d2b991aabef06fb67e2716
rspringAltsrc <roger.spring@altsrc.net>
2019-06-06 22:18:41 -0700

    add error example

--- src/operators.spec.ts
+++ src/operators.spec.ts
@@ -123,5 +123,21 @@ describe('Marble testing operators', () => {
         expect().nothing();
       });
     });
+
+    xit('should emit until value is > 5', () => {
+      testScheduler.run(helpers => {
+        const { cold, expectObservable } = helpers;
+
+        const source = '  -1--3---6---4---7--2------|';
+        const expected = '-1--3---6|';
+
+        const sut = cold(source).pipe(
+          takeWhile(x => x < 5)
+        );
+
+        expectObservable(sut).toBe(expected);
+        expect().nothing();
+      });
+    });
   });
 });
