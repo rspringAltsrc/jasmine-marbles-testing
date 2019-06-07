d37ea5a621095621009ff2609cd4e9516b0695de
rspringAltsrc <roger.spring@altsrc.net>
2019-06-06 21:23:06 -0700

    converted time spec

--- src/time.spec.ts
+++ src/time.spec.ts
@@ -1,30 +1,47 @@
 import { cold, getTestScheduler, hot } from 'jasmine-marbles';
+import { TestScheduler } from 'rxjs/testing';
 import { interval } from 'rxjs/observable/interval';
 import { of } from 'rxjs/observable/of';
 import { delay, filter, map, take } from 'rxjs/operators';
 
 describe('Marbe testing with time', () => {
+  let testScheduler: TestScheduler;
+  beforeEach(() => {
+    testScheduler = getTestScheduler();
+  })
+
   describe('Interval', () => {
     it('should keeps only even numbers', () => {
-      const source = interval(10, getTestScheduler()).pipe(
-        take(10),
-        filter(x => x % 2 === 0),
-      );
-      const expected = cold('-a-b-c-d-e|', { a: 0, b: 2, c: 4, d: 6, e: 8 });
+      testScheduler.run(helpers => {
+        const { cold, expectObservable } = helpers;
 
-      expect(source).toBeObservable(expected);
+        const obs1 = '-a-b-c-|';
+        const expected = '10ms a 19ms b 19ms c 19ms d 19ms e 9ms |';
+        const expectedValues = { a: 0, b: 2, c: 4, d: 6, e: 8 };
+
+        const sut = interval(10).pipe(
+          take(10),
+          filter(x => x % 2 === 0),
+        );
+
+        expectObservable(sut).toBe(expected, expectedValues);
+      });
     });
   });
 
   describe('Delay', () => {
     it('should waits 20 frames before receive the value', () => {
-      const scheduler = getTestScheduler();
-      const source = of('a').pipe(
-        delay(20, scheduler),
-      );
-      const expected = cold('--(a|)');
+      testScheduler.run(helpers => {
+        const { cold, expectObservable } = helpers;
 
-      expect(source).toBeObservable(expected);
+        const expected = '20ms (a|)';
+
+        const sut = of('a').pipe(
+          delay(20),
+        );
+
+        expectObservable(sut).toBe(expected);
+      });
     });
   });
 });
