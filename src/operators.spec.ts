20cc95d788423e10a1869db34abbf84d07a8ee95
rspringAltsrc <roger.spring@altsrc.net>
2019-06-06 21:07:26 -0700

    SwitchMap and MergeMap converted

--- src/operators.spec.ts
+++ src/operators.spec.ts
@@ -7,7 +7,7 @@ describe('Marble testing operators', () => {
   beforeEach(() => {
     testScheduler = getTestScheduler();
   })
-  
+
   describe('Map', () => {
     it('should add "1" to each value emitted', () => {
       const values = { a: 1, b: 2, c: 3, x: 2, y: 3, z: 4 };
@@ -32,25 +32,41 @@ describe('Marble testing operators', () => {
 
   describe('MergeMap', () => {
     it('should maps to inner observable and flattens', () => {
-      const values = { a: 'hello', b: 'world', x: 'hello world' };
-      const obs1 = cold('-a-------a--|', values);
-      const obs2 = cold('-b-b-b-|', values);
-      const expected = cold('--x-x-x---x-x-x-|', values);
+      testScheduler.run(helpers => {
+        const { cold, expectObservable } = helpers;
+        const values = { a: 'hello', b: 'world', x: 'hello world' };
+        const obs1 = '-a-------a--|';
+        const obs2 = '-b-b-b-|';
+        const expected = '--x-x-x---x-x-x-|';
 
-      const result = obs1.pipe(mergeMap(x => obs2.pipe(map(y => x + ' ' + y))));
-      expect(result).toBeObservable(expected);
+        const sut = cold(obs1, values).pipe(
+          switchMap(x => cold(obs2, values).pipe(
+            map(y => x + ' ' + y)
+          ))
+        );
+
+        expectObservable(sut).toBe(expected, values);
+      });
     });
   });
 
   describe('SwitchMap', () => {
     it('should maps each value to inner observable and flattens', () => {
-      const values = { a: 10, b: 30, x: 20, y: 40 };
-      const obs1 = cold('-a-----a--b-|', values);
-      const obs2 = cold('a-a-a|', values);
-      const expected = cold('-x-x-x-x-xy-y-y|', values);
+      testScheduler.run(helpers => {
+        const { cold, expectObservable } = helpers;
+        const values = { a: 10, b: 30, x: 20, y: 40 };
+        const obs1 = '-a-----a--b-|';
+        const obs2 = 'a-a-a|';
+        const expected = '-x-x-x-x-xy-y-y|';
 
-      const result = obs1.pipe(switchMap(x => obs2.pipe(map(y => x + y))));
-      expect(result).toBeObservable(expected);
+        const sut = cold(obs1, values).pipe(
+          switchMap((x: number) => cold(obs2, values).pipe(
+            map((y: number) => x + y)
+          ))
+        );
+
+        expectObservable(sut).toBe(expected, values);
+      });
     });
   });
 
@@ -58,14 +74,14 @@ describe('Marble testing operators', () => {
     it('should maps values to inner observable and emits in order', () => {
       testScheduler.run(helpers => {
         const { cold, expectObservable } = helpers;
-      const values = { a: 10, b: 30, x: 20, y: 40 };
-      const obs1 = '-a--------b------ab|';
-      const obs2 = 'a-a-a|'
-      const expected = '-x-x-x----y-y-y--x-x-xy-y-y|';
+        const values = { a: 10, b: 30, x: 20, y: 40 };
+        const obs1 = '-a--------b------ab|';
+        const obs2 = 'a-a-a|';
+        const expected = '-x-x-x----y-y-y--x-x-xy-y-y|';
 
         const sut = cold(obs1, values).pipe(
-          concatMap((x:number) => cold(obs2, values).pipe(
-            map((y:number) => x + y)
+          concatMap((x: number) => cold(obs2, values).pipe(
+            map((y: number) => x + y)
           ))
         );
 
